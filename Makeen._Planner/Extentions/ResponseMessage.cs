669bddf578e3996468d120501b6aaa91feaab4b4
محمدطه فرشیدفر <mtf.1380.2001@gmail.com>
2024-12-19 10:01:48 +0330

    Update UOW Added Response Message Class

---
+++ Makeen._Planner/Extentions/ResponseMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Makeen._Planner.Extentions
+{
+    public class ResponseMessage 
+    {
+        public object? Content { get; set; }
+
+        public string Message { get; set; } = "عملیات با موفقیت انجام شد";
+
+    }
+}
